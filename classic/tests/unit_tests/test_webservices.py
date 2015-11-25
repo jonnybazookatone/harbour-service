65a0107195787e4ff3cff8a54e7012d9dde21ecb
Jonny Elliott <jonathan.elliott@cfa.harvard.edu>
2015-11-25 14:00:54 -0500

    feature/aws-209
    started adding the skeleton content of the tests and also the docs
    in the end points.

--- classic/tests/unit_tests/test_webservices.py
+++ classic/tests/unit_tests/test_webservices.py
@@ -7,12 +7,37 @@ from classic import app
 from flask.ext.testing import TestCase
 from flask import url_for
 
+stub_classic_success = {
+    "email": "roman.chyla@gmail.com",
+    "cookie": "50eefa48dc",
+    "tmp_cookie": "",
+    "openurl_srv": "",
+    "openurl_icon": "",
+    "loggedin": "1",
+    "myadsid": "352401271",
+    "lastname": "",
+    "firstname": "",
+    "fullname": "",
+    "message": "LOGGED_IN",
+    "request": {
+        "man_cookie": "",
+        "man_email": "roman.chyla@gmail.com",
+        "man_nemail": "",
+        "man_passwd": "******",
+        "man_npasswd": "",
+        "man_vpasswd": "",
+        "man_name": "",
+        "man_url": "http://adsabs.harvard.edu",
+        "man_cmd": "4"
+        }
+}
+
 
 class TestEndpoints(TestCase):
     """
     Tests http endpoints
     """
-  
+
     def create_app(self):
         """
         Create the wsgi application
@@ -21,10 +46,30 @@ class TestEndpoints(TestCase):
         app_.config['CLASSIC_LOGGING'] = {}
         return app_
 
-    def test_authorise_endpoint(self):
+    @mock.patch('classic.views.requests.post',  mock.MagicMock(return_value=stub_classic_success))
+    def test_authenticating_workflow(self, mocked_request):
         """
-        Test basic functionality of the GithubListener endpoint
+        Tests the overall workflow of a user authenticating their ADS credentials via the web app
         """
-        url = url_for('AuthenticateUser'.lower())
-        r = self.client.get(url)
+
+        # 1. The user fills in their credentials
+        #    - email
+        #    - password
+        #    - mirror
+        user_data = {
+            'classic_email': 'user@ads.com',
+            'classic_password': 'password',
+            'mirror': 'mirror'
+        }
+
+        # 2. The user submits their credentials to the end point
+        url = url_for('AuthenticatedUser'.lower())
+        r = self.client.post(url, json=user_data)
         self.assertStatus(r, 200)
+
+        # 3. The webservice contacts the external ADS classic end point to confirm the credentials are correct
+        # 4. If the credentials are correct, they are stored in myADS
+        mocked_request.assert_has_calls(
+            mock.call(['http://adsabs.harvard.edu']),
+            mock.call(['http://api.adsabs.harvard.edu/v1/vault/user-data'])
+        )
--- classic/views.py
+++ classic/views.py
@@ -2,6 +2,8 @@
 Views
 """
 
+import requests
+
 from flask import current_app, request, abort
 from flask.ext.restful import Resource
 
@@ -11,5 +13,27 @@ class AuthenticateUser(Resource):
     End point to authenticate the user's ADS classic credentials with the external ADS Classic end point
     """
 
-    def get(self):
+    def post(self):
+        """
+        HTTP POST request that receives the user's ADS Classic credentials, and then contacts the Classic system to
+        check that what the user provided is indeed valid. If valid, the users ID is stored within the myADS service
+        store.
+
+        Post body:
+        ----------
+        KEYWORD, VALUE
+        classic_email: <string> ADS Classic e-mail of the user
+        classic_password: <string> ADS Classic password of the user
+        classic_mirror: <string> ADS Classic mirror this user belongs to
+
+        Return data:
+        -----------
+        classic_authed: <boolean> were they authenticated
+        classic_email: <string> e-mail that authenticated correctly, empty string on fail
+
+        HTTP Responses:
+        --------------
+        Succeed authentication: 200
+        Fail authentication: 404
+        """
         pass
