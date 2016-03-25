6410ad0edb412a5f975cfbce068851045e7699e4
Jonny Elliott <jonnynelliott@googlemail.com>
2016-03-25 09:16:56 -0400

    Scenario where classic does not give a description

--- harbour/config.py
+++ harbour/config.py
@@ -1,13 +1,28 @@
 ADS_CLASSIC_URL = 'http://{mirror}/email={email}&password={password}'
 ADS_CLASSIC_LIBRARIES_URL = 'http://{mirror}/cookie={cookie}'
-ADS_CLASSIC_MIRROR_LIST = ['adstrio.cfa.harvard.edu', 'adsnun.cfa.harvard.edu', 'adsate.cfa.harvard.edu',
-                           'astrobib.u-strasbg.fr', 'ads.nao.ac.jp', 'ads.astro.puc.cl', 'esoads.eso.org',
-                           'ukads.nottingham.ac.uk', 'ads.iucaa.ernet.in', 'ads.inasan.ru', 'ads.bao.ac.cn',
-                           'ads.mao.kiev.ua', 'ads.ari.uni-heidelberg.de', 'ads.arsip.lipi.go.id', 'ads.on.br',
-                           'saaoads.chpc.ac.za', 'adsabs.harvard.edu']
+ADS_CLASSIC_MIRROR_LIST = [
+    'adstrio.cfa.harvard.edu',
+    'adsnun.cfa.harvard.edu',
+    'adsate.cfa.harvard.edu',
+    'astrobib.u-strasbg.fr',
+    'ads.nao.ac.jp',
+    'ads.astro.puc.cl',
+    'esoads.eso.org',
+    'ukads.nottingham.ac.uk',
+    'ads.iucaa.ernet.in',
+    'ads.inasan.ru',
+    'ads.bao.ac.cn',
+    'ads.mao.kiev.ua',
+    'ads.ari.uni-heidelberg.de',
+    'ads.arsip.lipi.go.id',
+    'ads.on.br',
+    'saaoads.chpc.ac.za',
+    'adsabs.harvard.edu'
+]
 ADS_TWO_POINT_OH_S3_MONGO_BUCKET = 'adsabs-mongogut'
 ADS_TWO_POINT_OH_LOADED_USERS = False
 ADS_TWO_POINT_OH_USERS = {}
+ADS_TWO_POINT_OH_MIRROR = 'adsabs.harvard.edu'
 
 SQLALCHEMY_BINDS = {'harbour': ''}
 
